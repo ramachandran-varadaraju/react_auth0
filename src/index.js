ac48a64a4a92f246ccd5ba4918f5e9769d6b580d
Ramachandran Varadaraju <rajram92@hotmail.com>
2022-01-15 17:45:18 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
+console.log(window.location.origin);
+
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
